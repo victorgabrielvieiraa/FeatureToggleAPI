33eeb6b44e7d88a60a77fcf77973d16550121be0
victorgabrielvieiraa <88777214+victorgabrielvieiraa@users.noreply.github.com>
2024-11-28 10:30:44 -0300

    Criação da model e do dbContext

---
+++ FeatureToggle/Data/FeatureDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FeatureToggle.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace FeatureToggle.Data
+{
+    public class FeatureDbContext : DbContext
+    {
+
+        public FeatureDbContext(DbContextOptions<FeatureDbContext> options) : base(options) { }
+
+        public DbSet<Feature> Features { get; set; }
+
+    }
+}
---
+++ FeatureToggle/Models/Feature.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FeatureToggle.Models
+{
+    public class Feature
+    {
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Enabled { get; set; }
+        public bool Android { get; set; }
+        public bool Ios { get; set; }
+        public bool Web { get; set; }
+
+    }
+}
